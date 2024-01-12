ac31f7df0c5f09a5fc32d26d20446c81f86f4a11
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2024-01-12 12:35:03 -0500

    player score cards correctly display players

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,15 @@ import useGameContext from '../assets/theme/context';
 const Footer = () => {
 
     const { playerOne, playerTwo, playerTwoCPU, scores} = useGameContext()
+
+    const determinePlayerLabels = (symbol) => {
+      if(playerTwoCPU && playerOne.symbol === symbol) { return 'YOU' }
+      if(playerTwoCPU && playerTwo.symbol === symbol) { return 'CPU' }
+      if(!playerTwoCPU && playerOne.symbol === symbol) { return 'P1' }
+      if(!playerTwoCPU && playerTwo.symbol === symbol) { return 'P2' }  
+// (playerTwoCPU && playerOne.symbol === 'X') ? 'YOU' : playerOne.symbol === 'O' ? 'P2' : 'P1'
+    }
+
   return (
     <>
         <GridItem
@@ -19,7 +28,8 @@ const Footer = () => {
           alignItems="center"
           textStyle="subp"
 
-        >X ({playerTwoCPU ? 'YOU' : playerOne.symbol === 'O' ? 'P2' : 'P1'}) <Box textStyle="h3" fontWeight="normal">{scores.p1}</Box></GridItem>
+        // >X ({(playerTwoCPU && playerOne.symbol === 'X') ? 'YOU' : playerOne.symbol === 'O' ? 'P2' : 'P1'}) <Box textStyle="h3" fontWeight="normal">{scores.p1}</Box></GridItem>
+        >X ({determinePlayerLabels('X')}) <Box textStyle="h3" fontWeight="normal">{scores.p1}</Box></GridItem>
         <GridItem
           w="96px"
           h="64px"
@@ -43,7 +53,7 @@ const Footer = () => {
           justifyContent="center"
           alignItems="center"
           textStyle="subp"
-        >O ({playerTwoCPU ? 'CPU' : playerTwo.symbol === 'O' ? 'P2' : 'P1'}) <Box textStyle="h3" fontWeight="normal">{scores.p2}</Box></GridItem>
+        >O ({determinePlayerLabels('O')}) <Box textStyle="h3" fontWeight="normal">{scores.p2}</Box></GridItem>
     </>
   )
 }
